49242cbcada7506fe7ece936148383d3dd22ea3b
Jason Soo <wwwjscom@gmail.com>
2014-02-10 17:13:58 -0500

    Now sorting tokens using a Lexicon

--- scanner/actions/print.go
+++ scanner/actions/print.go
@@ -8,6 +8,7 @@ import "regexp"
 import log "github.com/cihub/seelog"
 import filereader "github.com/wwwjscom/ocr_engine/scanner/filereader"
 import filewriter "github.com/wwwjscom/ocr_engine/scanner/filewriter"
+import lexicon "github.com/wwwjscom/ocr_engine/lexicon"
 
 func PrintTokens() *print_tokens_action {
     return new(print_tokens_action)
@@ -42,6 +43,8 @@ func (a *print_tokens_action) DefineFlags(fs *flag.FlagSet) {
 
 func (a *print_tokens_action) Run() {
     SetupLogging(*a.verbosity)
+    
+    lex := lexicon.NewLexicon()
 
     writer := new(filewriter.TrecFileWriter)
     writer.Init(*a.tokenOutputPath)
@@ -55,16 +58,25 @@ func (a *print_tokens_action) Run() {
     for doc := range docStream {
 
         for t := range doc.Tokens() {
-            log.Tracef("Adding token: %s", t)
-            writer.StringChan <- &t.Text
+            log.Tracef("Adding token: %s", t)            
+            lex.Add(t.Text)            
         }
 
         log.Debugf("Document %s (%d tokens)\n", doc.Identifier(), doc.Len())
     }
-
+    
     log.Info("Done reading from the docStream")
     close(writer.StringChan)
-
+    
+    log.Debug("Sorting Words")
+    lex.Sort()
+    
+    log.Debug("Writing Words")
+    // Write out all the words
+    for _, w := range lex.Words() {
+      writer.StringChan <- &w
+    }
+    
     // Wait for the writer to finish
     writer.Wait()
 }
